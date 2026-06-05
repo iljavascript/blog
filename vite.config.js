cb9f7380877d94ed171a2e3f73ae2fd5319db109
iljavascript <iluelyar@gmail.com>
2026-06-05 21:42:05 +0800

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "./",
+  base: "/", 
   plugins: [react()],
   resolve: {
     alias: {
@@ -12,9 +12,5 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-  },
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
   },
 });
